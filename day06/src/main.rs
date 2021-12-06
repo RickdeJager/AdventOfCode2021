a1c3bfb3dc51fba924308afe991e0b084d28dc9d
Rick de Jager <rickdejager99@gmail.com>
2021-12-06 22:11:18 +0100

    Add basic perf stats

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -20,6 +20,12 @@ fn main() {
         .map(|c| usize::from_str_radix(c, 10))
         .collect::<Result<Vec<usize>, _>>()
         .expect("invalid input");
-    println!("Part 1: {}", solve(&input, 80));
-    println!("Part 1: {}", solve(&input, 256));
+    let t = std::time::Instant::now();
+    let part1 = solve(&input, 80);
+    let time_p1 = t.elapsed().as_secs_f64() * 1e6;
+    let t = std::time::Instant::now();
+    let part2 = solve(&input, 256);
+    let time_p2 = t.elapsed().as_secs_f64() * 1e6;
+    println!("Part 1: {:20} (took {:2.3} μs)", part1, time_p1);
+    println!("Part 2: {:20} (took {:2.3} μs)", part2, time_p2);
 }
