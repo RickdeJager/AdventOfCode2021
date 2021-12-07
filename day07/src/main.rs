c29574eefa1ed5d791d1c29ada91d7e45a30b578
Rick de Jager <rickdejager99@gmail.com>
2021-12-07 10:20:42 +0100

    Add day 7

---
+++ day07/src/main.rs
@@ -0,0 +1,31 @@
+fn part1(inp: &[isize]) -> isize {
+    inp.iter()
+        .map(|dest| inp.iter().map(|loc| (dest - loc).abs()).sum())
+        .min()
+        .unwrap()
+}
+
+fn part2(inp: &[isize]) -> isize {
+    inp.iter()
+        .map(|dest| {
+            inp.iter()
+                .map(|loc| {
+                    let dist = (dest - loc).abs();
+                    (dist * (dist + 1)) / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap()
+}
+
+fn main() {
+    let input = include_str!("input.txt")
+        .trim()
+        .split(',')
+        .map(|c| c.parse())
+        .collect::<Result<Vec<isize>, _>>()
+        .expect("invalid input");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+}
